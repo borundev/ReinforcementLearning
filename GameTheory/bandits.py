fc88d36c1f4447a2ddf882bb9eb2df4791e7c31c
borundev <borundev@gmail.com>
2022-02-22 00:49:35 +0000

    Allowed for odd number of bandits

--- GameTheory/bandits.py
+++ GameTheory/bandits.py
@@ -4,7 +4,8 @@ import wandb
 from torch import nn
 from tqdm import tqdm
 
-class CompetingBandit(nn.Module):
+
+class CompetingBanditGradient(nn.Module):
 
     def __init__(self, payoff_matrix, num_bandits, epsilon=.01):
         super().__init__()
@@ -18,7 +19,7 @@ class CompetingBandit(nn.Module):
         self.softmax = nn.Softmax(dim=-1)
         self.rs = np.zeros(self.num_bandits)
         self.opt = torch.optim.Adam(self.parameters(), lr=.1)
-        self.epsilon=epsilon
+        self.epsilon = epsilon
 
     @property
     def pi(self):
@@ -30,7 +31,7 @@ class CompetingBandit(nn.Module):
         if np.random.uniform() > self.epsilon:
             return (np.random.uniform(0, 1, [self.num_bandits, 1]) > self.pi.cumsum(1)).sum(1)
         else:
-            return np.random.choice(range(self.num_actions),self.num_bandits)
+            return np.random.choice(range(self.num_actions), self.num_bandits)
 
     def get_rewards(self, actions):
         # logic of inverting shuffle based on https://stackoverflow.com/questions/26577517/inverse-of-random-shuffle
@@ -46,27 +47,110 @@ class CompetingBandit(nn.Module):
         unshuf_order = np.zeros_like(shuf_order)
         unshuf_order[shuf_order] = np.arange(self.num_bandits)
 
-        unshuffled_actions = shuffled_actions[unshuf_order]  # Unshuffle the shuffled data
+        # unshuffled_actions = shuffled_actions[unshuf_order]  # Unshuffle the shuffled data
         # assert np.all(np.equal(unshuffled_actions, actions))
+
+        shuffled_mask = np.ones(self.num_bandits, dtype=np.bool)
+        if self.num_bandits % 2 == 1:
+            shuffled_mask[-1] = False
+            shuffled_actions = shuffled_actions[:-1]
 
         paired_actions = shuffled_actions.reshape(self.num_bandits // 2, 2)
         rows = paired_actions[:, 0]
         cols = paired_actions[:, 1]
 
-        shuffled_rewards = self.payoff_matrix[rows, cols].reshape(self.num_bandits, )
+        shuffled_rewards = self.payoff_matrix[rows, cols].reshape(self.num_bandits >> 1 << 1, )
         # assert all([k in payoff_matrix.sum(-1).flatten() for k in shuffled_rewards.reshape(-1,2).sum(1) ])
 
+        mask = shuffled_mask[unshuf_order]
         # return unshuffled rewards
+        if self.num_bandits % 2 == 1:
+            shuffled_rewards = np.concatenate([shuffled_rewards, [201]])
         rewards = shuffled_rewards[unshuf_order]
-        return rewards
 
-    def take_rewards(self, r, actions):
-        r_diff = r - self.rs
+        return rewards, mask
+
+    def take_rewards(self, r, actions, mask):
+        idx = np.arange(self.num_bandits)[mask]
+        actions = actions[mask]
+        r = r[mask]
+
+        r_diff = r - self.rs[idx]
+        if not mask.all():
+            tmp = self.H.data.clone()
+
         self.opt.zero_grad()
-        self.H.grad.data = torch.tensor((r_diff[:, None] * self.pi))
-        self.H.grad.data[np.arange(self.num_bandits), actions] -= torch.tensor(r_diff)
+        self.H.grad.data[idx] = torch.tensor((r_diff[:, None] * self.pi[idx]))
+        self.H.grad.data[idx, actions] -= torch.tensor(r_diff)
         self.opt.step()
-        self.rs += 1.0 / 5 * r_diff
+        if not mask.all():
+            self.H.data[~mask] = tmp[~mask]
+        self.rs[idx] += 1.0 / 5 * r_diff
+
+
+class CompetingBanditUCB(nn.Module):
+
+    def __init__(self, payoff_matrix, num_bandits, epsilon=.01):
+        super().__init__()
+        self.payoff_matrix = payoff_matrix
+        self.num_actions = len(self.payoff_matrix)
+        self.num_bandits = num_bandits
+        self.epsilon = epsilon
+
+        self.qs = np.zeros([self.num_bandits, self.num_actions])
+        self.steps = np.zeros([self.num_bandits, self.num_actions])
+        self.total_steps = np.zeros([self.num_bandits, 1])
+
+    def play_one(self):
+        if (self.steps > 0).all():
+            return np.argmax(self.qs + np.sqrt(np.log(self.total_steps) / (self.steps + 1e-5)), -1)
+        else:
+            return np.random.randint(0, self.num_actions, self.num_bandits)
+
+    def get_rewards(self, actions):
+        # logic of inverting shuffle based on https://stackoverflow.com/questions/26577517/inverse-of-random-shuffle
+
+        # shuffle the actions of agents and pair them to get rewards
+
+        # Here we create an array of shuffled indices
+        shuf_order = np.arange(self.num_bandits)
+        np.random.shuffle(shuf_order)
+        shuffled_actions = actions[shuf_order]  # Shuffle the original data
+
+        # Create an inverse of the shuffled index array (to reverse the shuffling operation, or to "unshuffle")
+        unshuf_order = np.zeros_like(shuf_order)
+        unshuf_order[shuf_order] = np.arange(self.num_bandits)
+
+        # unshuffled_actions = shuffled_actions[unshuf_order]  # Unshuffle the shuffled data
+        # assert np.all(np.equal(unshuffled_actions, actions))
+
+        shuffled_mask = np.ones(self.num_bandits, dtype=np.bool)
+        if self.num_bandits % 2 == 1:
+            shuffled_mask[-1] = False
+            shuffled_actions = shuffled_actions[:-1]
+
+        paired_actions = shuffled_actions.reshape(self.num_bandits // 2, 2)
+        rows = paired_actions[:, 0]
+        cols = paired_actions[:, 1]
+
+        shuffled_rewards = self.payoff_matrix[rows, cols].reshape(self.num_bandits >> 1 << 1, )
+        # assert all([k in payoff_matrix.sum(-1).flatten() for k in shuffled_rewards.reshape(-1,2).sum(1) ])
+
+        mask = shuffled_mask[unshuf_order]
+        # return unshuffled rewards
+        if self.num_bandits % 2 == 1:
+            shuffled_rewards = np.concatenate([shuffled_rewards, [201]])
+        rewards = shuffled_rewards[unshuf_order]
+
+        return rewards, mask
+
+    def take_rewards(self, r, actions, mask):
+        idx = np.arange(self.num_bandits)[mask]
+        actions = actions[mask]
+        r = r[mask]
+        self.steps[idx, actions] += 1
+        self.total_steps[idx] += 1
+        self.qs[idx, actions] += 1.0 / 50 * (r - self.qs[idx, actions])
 
 
 if __name__ == '__main__':
@@ -82,14 +166,14 @@ if __name__ == '__main__':
     )}
 
     game = 'Rock Paper Scissor'
-    cb = CompetingBandit(payoff_matrix[game], 2)
+    cb = CompetingBanditGradient(payoff_matrix[game], 3)
     wandb.init(project="Competing Bandits", entity="borundev", name = game)
 
     for s in tqdm(range(10000)):
         try:
             actions = cb.play_one()
-            r = cb.get_rewards(actions)
-            cb.take_rewards(r, actions)
+            r,m = cb.get_rewards(actions)
+            cb.take_rewards(r, actions,m)
             d = dict(zip(map(lambda x: f'Prob Action {x}',range(1,cb.num_actions+1)),cb.pi.mean(0)))
             wandb.log(d,step=s)
             d = dict(zip(map(lambda x: f'Prob Agent 1 Action {x}', range(1, cb.num_actions + 1)), cb.pi[0]))
